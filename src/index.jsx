4f4981810b52ac103c93c097b731ec57b74841d5
naoko.tamagaki <166595834+tamasshu@users.noreply.github.com>
2024-11-08 14:56:39 +0900

    不要なファイルの削除・初期ファイルの修正

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+reportWebVitals();
